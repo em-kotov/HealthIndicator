1a4c3aa8baf176044680e3014e6f5b76a03205e3
em-kotov <123034420+em-kotov@users.noreply.github.com>
2024-10-27 14:23:45 -0400

    max and min are now in health class

--- Scripts/Health.cs
+++ Scripts/Health.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField, Range(0f, 100f)] private float _maxPoints = 100;
+
     private float _points;
+    private float _minPoints = 0;
 
     public event Action<float> PointsChanged;
+
+    private void Start()
+    {
+        SetCurrentHealth(_maxPoints);
+    }
 
     public void SetCurrentHealth(float points)
     {
@@ -13,25 +21,25 @@ public class Health : MonoBehaviour
         InvokePointsChanged();
     }
 
-    public void LoosePoints(float lostPoints, float minPoints, float maxPoints)
+    public void LoosePoints(float lostPoints)
     {
         if (IsNegative(lostPoints))
             return;
 
-        SetCurrentHealth(GetClampedPoints(_points - lostPoints, minPoints, maxPoints));
+        SetCurrentHealth(GetClampedPoints(_points - lostPoints));
     }
 
-    public void AddPoints(float addedPoints, float minPoints, float maxPoints)
+    public void AddPoints(float addedPoints)
     {
         if (IsNegative(addedPoints))
             return;
 
-        SetCurrentHealth(GetClampedPoints(_points + addedPoints, minPoints, maxPoints));
+        SetCurrentHealth(GetClampedPoints(_points + addedPoints));
     }
 
-    private float GetClampedPoints(float points, float minPoints, float maxPoints)
+    private float GetClampedPoints(float points)
     {
-        return Mathf.Clamp(points, minPoints, maxPoints);
+        return Mathf.Clamp(points, _minPoints, _maxPoints);
     }
 
     private bool IsNegative(float value)
--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,16 +4,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private InputHandler _inputHandler;
     [SerializeField] private Health _health;
-    [SerializeField, Range(0f, 100f)] private float _maxPoints = 100;
     [SerializeField, Range(0f, 100f)] private float _addedPoints = 10;
     [SerializeField, Range(0f, 100f)] private float _lostPoints = 10;
-
-    private float _minPoints = 0;
-
-    private void Start()
-    {
-        _health.SetCurrentHealth(_maxPoints);
-    }
 
     private void OnEnable()
     {
@@ -29,11 +21,11 @@ public class Player : MonoBehaviour
 
     private void HandleHit()
     {
-        _health.LoosePoints(_lostPoints, _minPoints, _maxPoints);
+        _health.LoosePoints(_lostPoints);
     }
 
     private void HandleHeal()
     {
-        _health.AddPoints(_addedPoints, _minPoints, _maxPoints);
+        _health.AddPoints(_addedPoints);
     }
 }
